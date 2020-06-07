5e7caaba8866a3424d40b664091750d839cdd4b4
Byulyant Hasanov <8962958+balito@users.noreply.github.com>
2020-06-08 00:58:51 +0200

    build

---
+++ build/precache-manifest.13fb29e56969c45d8b89a2b20b4428af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd79b569d703116af15bae45c5af0e1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9904308d1bb698e6e3bd",
+    "url": "/static/css/main.0fba0c73.chunk.css"
+  },
+  {
+    "revision": "12b53d15e2b54649dad7",
+    "url": "/static/js/2.fd9fb42f.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.fd9fb42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9904308d1bb698e6e3bd",
+    "url": "/static/js/main.bdd8c6db.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
